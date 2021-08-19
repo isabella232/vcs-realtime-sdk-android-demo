b286356aec5e0e1e04c4ae7cf794bdfd29a3317c
dchappelle <douglas.chappelle@unify.com>
2021-08-19 17:11:36 -0400

    Password in settings shouldn't be clear text

--- app/src/main/java/net/atos/vcs/realtime/demo/SettingsFragment.kt
+++ app/src/main/java/net/atos/vcs/realtime/demo/SettingsFragment.kt
@@ -1,10 +1,22 @@
 package net.atos.vcs.realtime.demo
 
 import android.os.Bundle
+import android.text.InputType
+import android.view.View
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pw = preferenceScreen.preferenceManager.findPreference<EditTextPreference>("password")
+        pw?.setOnBindEditTextListener {
+            it.inputType = InputType.TYPE_CLASS_TEXT + InputType.TYPE_TEXT_VARIATION_PASSWORD
+        }
     }
 }
