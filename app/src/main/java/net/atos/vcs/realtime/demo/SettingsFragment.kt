97fb0e1ef8497da2affb31d8523b4170765c8e86
dchappelle <douglas.chappelle@unify.com>
2021-10-08 14:30:35 -0400

    Added action bar to Settings screen

--- app/src/main/java/net/atos/vcs/realtime/demo/SettingsFragment.kt
+++ app/src/main/java/net/atos/vcs/realtime/demo/SettingsFragment.kt
@@ -3,8 +3,10 @@ package net.atos.vcs.realtime.demo
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -13,6 +15,19 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        toolbar?.apply {
+            title = "Settings"
+            setTitleTextColor(resources.getColor(R.color.deep_blue))
+            isTitleCentered = true
+            setNavigationIcon(R.drawable.action_back)
+            setNavigationIconTint(resources.getColor(R.color.deep_blue))
+            setNavigationOnClickListener {
+                val action = SettingsFragmentDirections.actionSettingsFragmentToSignInFragment()
+                findNavController().navigate(action)
+            }
+        }
 
         val pw = preferenceScreen.preferenceManager.findPreference<EditTextPreference>("password")
         pw?.setOnBindEditTextListener {
